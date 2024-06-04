607ccc1e1f72ad77f6235b63e21b6c2abbf58889
Ewok Ozwok <138328112+EwokOzwok@users.noreply.github.com>
2024-06-04 10:38:29 -0400

    Update Initial R Libraries.R

--- Initial R Libraries.R
+++ Initial R Libraries.R
@@ -1,1 +1,7 @@
 install.packages(c("reticulate", "psych", "dplyr", "caret", "caTools", "finalfit"), dependencies=T)
+
+
+reticulate::conda_create(envname = "mlbase", 
+                         packages = c("scikit-learn", "matplotlib", "seaborn", "tensorflow"), 
+                         pip = T, 
+                         python_version = '3.8')
